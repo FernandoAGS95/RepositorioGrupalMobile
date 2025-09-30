293fdb2af6d3b27448f90b4f3f0573c92319341b
nico-carvajalm <nico.carvajalm@duocuc.cl>
2025-09-30 02:09:58 -0300

    Inicio de proyecto + compose activado + home minima

---
+++ app/src/main/java/com/example/app_grupo7/ui/screens/HomeScreen.kt
@@ -0,0 +1,36 @@
+package com.example.app_grupo7.ui.screens
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*    // <- importa TODO material3, incluye TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Perfumería") }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Text("Bienvenido a AppPerfumes")
+            Button(onClick = { /* luego: navegar a catálogo */ }) {
+                Text("Explorar perfumes")
+            }
+        }
+    }
+}
